587b7f50c690e1e2ba90546b51b2c803fe1f07e5
Alexandre Sumoyama <alexandre.sumoyama@gmail.com>
2023-09-11 19:10:44 -0300

    improve design

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -2,23 +2,19 @@ import React, { useState, MouseEvent } from 'react';
 import './Modal.css';
 import { ModalProps } from '../types/type';
 
-const Modal: React.FC<ModalProps>  = ({id, visible, paintDiv}) => {
-  const [color, setColor] = useState('');
-
-
+const Modal: React.FC<ModalProps> = ({ id, visible, paintDiv }) => {
   return (
     <>
-    {
-      visible && (   
-      <div className='modal' style={{backgroundColor: color}}>
-        <button onClick={(event)=> paintDiv(id, 'red')}>Vermelho</button>
-        <button onClick={(event)=> paintDiv(id, 'green')}>Verde</button>
-        <button onClick={(event)=> paintDiv(id, 'blue')}>Azul</button>
+      {
+        visible && (
+          <div className='modal' >
+            <button onClick={() => paintDiv(id, 'red')} className='colors' id='red'></button>
+            <button onClick={() => paintDiv(id, 'green')} className='colors' id='green'></button>
+            <button onClick={() => paintDiv(id, 'blue')} className='colors' id='blue'></button>
+          </div >
+        )}
 
-    </div>
-    )}
-  
-  </>
+    </>
   );
 };
 
